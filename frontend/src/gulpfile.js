73e7d4d4d0e508bdd5808d24560d2220616a4a99
Reuben Bradley <reuben.bradley@neueda.com>
2017-05-24 17:06:53 +0100

    Added frontend docker container for building client application
    Outlined basic gulp tasks involved in build process

---
+++ frontend/src/gulpfile.js
@@ -0,0 +1,122 @@
+/* 
+ * Gulp configuration and task file.
+ * 
+ * @author Reuben Bradley
+ */
+
+'use strict';
+
+var config = {
+    outputDir: '/build/',
+    sourcePaths: {
+        styles: ['./css/**/*.scss'],
+        scripts: ['./js/**/*.js']
+    },
+    styles: {
+        bundles: [
+            { entry: './css/main.scss', outputFilename: 'main' }
+        ],
+        autoprefixerSettings: {
+            browsers: ['last 2 versions'],
+            cascade: false
+        }
+    },
+    scripts: {
+        webpackSettings: {
+            watch: false,
+            entry: {
+                main: './js/main.js'
+            },
+            output: {
+                filename: '[name].js'
+            }
+        }
+    }
+};
+var gulp = require('gulp');
+var prefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var chalk = require('chalk');
+var webpack = require('webpack-stream');
+
+var logSuccess = function ( text ) {
+    console.log(chalk.bgGreen.white('Task Runner: ' + text));
+};
+var logFailure = function ( text ) {
+    console.log(chalk.bgRed.white('Task Runner: ' + text));
+};
+
+// Individual tasks for image compression, styles, and scripts for easier
+//  customisation
+gulp.task('styles', function ( taskComplete ) {
+    var task, currentBundle;
+    var promises = [];
+    var bundles = config.styles.bundles;
+    
+    logSuccess('Starting \'styles\' task.');
+    for ( var index = 0, ln = bundles.length; index < ln; index++ ) {
+        task = new Promise(function ( resolve, reject ) {
+            logSuccess('Starting bundle: ' + bundles[index].entry);
+            currentBundle = bundles[index];
+            var stream = gulp.src(bundles[index].entry)
+                    .pipe(sass().on('error', sass.logError))
+                    .pipe(prefixer(config.styles.autoprefixerSettings))
+                    .pipe(rename(function (path) {
+                        path.basename = currentBundle.outputFilename;
+                    }))
+                    .pipe(gulp.dest(config.outputDir + 'css/'));
+
+            stream.on('error', function () {
+                logFailure('Stylesheet failed.');
+                reject();
+            })
+            .on('end', function () {
+                logSuccess('Stylesheet completed - ' + 
+                    currentBundle.entry + ' => ' + config.outputDir + 
+                    currentBundle.outputFilename + '.css');
+                resolve();
+            });
+        });
+        
+        promises.push(task);
+    }
+    
+    if ( promises.length ) {
+        Promise.all(promises).then(function () {
+            taskComplete();
+        }, function () {
+            taskComplete('An error occurred.');
+        });
+    }
+    else {
+        taskComplete('No style bundles given.');
+    }
+});
+
+gulp.task('scripts', function ( taskComplete ) {
+    var stream = gulp.src([])
+            .pipe(webpack(config.scripts.webpackSettings))
+            .pipe(gulp.dest(config.outputDir + 'js/'));
+
+    stream.on('error', function () {
+        logFailure('Scripts failed.');
+        taskComplete('Scripts failed.');
+    })
+    .on('end', function () {
+        logSuccess('Scripts completed successfully.');
+        taskComplete();
+    });
+});
+
+
+// Build and watch tasks for production and development
+gulp.task('build', [
+    'styles',
+    'scripts'
+]);
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch(config.sourcePaths.styles, ['styles']);
+    gulp.watch(config.sourcePaths.scripts, ['scripts']);
+});
---
+++ frontend/src/js/main.js
@@ -0,0 +1,9 @@
+/* 
+ * Entry point to the frontend javascript application.
+ * 
+ * @author Reuben Bradley <reuben.m.bradley@gmail.com>
+ */
+
+(function() {
+    alert('Bienvenue world!');
+})();
